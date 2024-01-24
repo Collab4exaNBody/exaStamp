d1e7e0564d0e1f41d8a09c73ae3728369839e0ed
Thierry Carrard <thierry.carrard@cea.fr>
2024-01-24 13:54:39 +0100

    removed analytics transfered to exaNBody

--- src/mechanical/mechanical_write_paraview.cpp
+++ src/mechanical/mechanical_write_paraview.cpp
@@ -51,10 +51,10 @@ namespace exaStamp
     ADD_SLOT( StringList  , fields               , INPUT , StringList({".*"}) , DocString{"List of regular expressions to select fields to project"} );
 
     ADD_SLOT( GridParticleLocalMechanicalMetrics, local_mechanical_data , INPUT, OPTIONAL );
-    ADD_SLOT( GridParticleLocalStructuralMetrics, local_structural_data , INPUT_OUTPUT, OPTIONAL);
+    ADD_SLOT( GridParticleLocalStructuralMetrics, local_structural_data , INPUT, OPTIONAL);
 
-    template<class... GridFields>
-    inline void execute_on_fields( const GridFields& ... grid_fields) 
+    template<class FieldSelectorFuncT, class... GridFields>
+    inline void execute_on_fields( const FieldSelectorFuncT& field_selector, const GridFields& ... grid_fields) 
     {
       {
         int s=0;
@@ -63,9 +63,6 @@ namespace exaStamp
         ldbg << std::endl;
       }
     
-      const auto& flist = *fields;
-      auto field_selector = [&flist] ( const std::string& name ) -> bool { for(const auto& f:flist) if( std::regex_match(name,std::regex(f)) ) return true; return false; } ;
-
       using CellParticleAcessor = GridParticleFieldAccessor<typename GridT::CellParticles * const>;
       CellParticleAcessor cells = { grid->cells() };
       ParaviewWriteTools::write_particles(ldbg,*mpi,*grid,cells,*domain,*filename,field_selector,*compression,*binary_mode,*write_box,*write_external_box,*write_ghost, grid_fields ... );
@@ -76,6 +73,9 @@ namespace exaStamp
     {
       int rank=0;
       MPI_Comm_rank(*mpi, &rank);
+      
+      const auto& flist = *fields;
+      auto field_selector = [&flist] ( const std::string& name ) -> bool { for(const auto& f:flist) if( std::regex_match(name,std::regex(f)) ) return true; return false; } ;
 
       VelocityNorm2Combiner vnorm2 = {};
       ProcessorRankCombiner processor_id = { {rank} };
@@ -85,22 +85,44 @@ namespace exaStamp
       //KineticEnergyTensorCombiner mv2tensor = { { species->data() , 0 } }; // too big for full outputs
 
       const CellParticleLocalMechanicalMetrics * __restrict__ mech_data = nullptr;
-      if( local_mechanical_data.has_value() ) mech_data = local_mechanical_data->data();
-      auto defgrad = mechanical_field(mech_data,field::defgrad);
+      if( local_mechanical_data.has_value() )
+      {
+        mech_data = local_mechanical_data->data();
+      }
+      else
+      {
+        if( field_selector("defgrad") || field_selector("greenlag") || field_selector("rot") || field_selector("stretch") )
+        {
+          fatal_error() << "requested a mechanical field but mechanical data is null" << std::endl;
+        }
+      }
+
+      auto defgrad  = mechanical_field(mech_data,field::defgrad);
       auto greenlag = mechanical_field(mech_data,field::greenlag);
-      auto rot     = mechanical_field(mech_data,field::rot);
-      auto stretch = mechanical_field(mech_data,field::stretch);
+      auto rot      = mechanical_field(mech_data,field::rot);
+      auto stretch  = mechanical_field(mech_data,field::stretch);
 
       const CellParticleLocalStructuralMetrics * __restrict__ structural_data = nullptr;
-      if( local_structural_data.has_value() ) structural_data = local_structural_data->data();
+      if( local_structural_data.has_value() )
+      {
+        structural_data = local_structural_data->data();
+      }
+      else
+      {
+        if( field_selector("crystal") )
+        {
+          fatal_error() << "requested a structural field but structural data is null" << std::endl;
+        }
+      }
       auto crystal = structural_field( structural_data , field::crystal );
 
-      execute_on_fields( processor_id, vnorm2, mv2, mass, momentum, defgrad, greenlag, rot, stretch, crystal, onika::soatl::FieldId<fid>{} ... );
+      execute_on_fields( field_selector, processor_id, vnorm2, mv2, mass, momentum, defgrad, greenlag, rot, stretch, crystal, onika::soatl::FieldId<fid>{} ... );
     }
 
   public:
     inline void execute() override final
     {
+      ldbg << "mechanical_write_paraview: write to "<< (*filename) <<std::endl;
       execute_on_field_set(grid->field_set);
     }
 
