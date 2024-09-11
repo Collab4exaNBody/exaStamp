5cc8f58b1d92ae550e3be20f1378c850037b2667
Thierry Carrard <thierry.carrard@cea.fr>
2024-09-11 14:43:03 +0200

    fixed compile errors

--- src/potential/snaplegacy/snaplegacy.cpp
+++ src/potential/snaplegacy/snaplegacy.cpp
@@ -217,6 +217,44 @@ namespace exaStamp
         this->operator () ( n,buf,en,fx,fy,fz,type, virial, cells , locks, lock_a);
       }
 
+      template<class CellsAccessorT>
+      inline void operator ()
+        (
+        size_t n,
+        ComputeBuffer& buf,
+        double& en,
+        double& fx,
+        double& fy,
+        double& fz,
+        unsigned int type, // to recover particle type
+        CellsAccessorT cells
+        ) const
+      {
+        FakeMat3d virial;
+        ComputePairOptionalLocks<false> locks = {};
+        FakeParticleLock lock_a = {};
+        this->operator () ( n,buf,en,fx,fy,fz,type,virial, cells, locks , lock_a );
+      }
+
+      template<class CellsAccessorT>
+      inline void operator ()
+        (
+        size_t n,
+        ComputeBuffer& buf,
+        double& en,
+        double& fx,
+        double& fy,
+        double& fz,
+        unsigned int type, // to recover particle type
+        Mat3d& virial ,
+        CellsAccessorT cells
+        ) const
+      {
+        ComputePairOptionalLocks<false> locks = {};
+        FakeParticleLock lock_a = {};
+        this->operator () ( n,buf,en,fx,fy,fz,type,virial, cells, locks , lock_a );
+      }
+
       template<class CellsAccessorT, class Mat3dT,class GridCellLocksT, class ParticleLockT>
       inline void operator ()
         (
