72aa9c8e42b525ba0d2ad8dc6f9b0d7ccbb03fa5
Paul Lafourcade <paul.lafourcade@cea.fr>
2025-07-25 10:57:27 +0200

    Modified script: add license only if header is not present. + check_license duplication script

--- scripts/check_license.py
+++ scripts/check_license.py
@@ -14,22 +14,6 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements. See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership. The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License. You may obtain a copy of the License at
-#   http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations
-# under the License.
-#
 import os, sys
 
 # Define the license headers
@@ -49,7 +33,6 @@ KIND, either express or implied. See the License for the
 specific language governing permissions and limitations
 under the License.
 */
-
 """
 
 license_header_python_style = """#
@@ -82,19 +65,13 @@ for foldername, subfolders, filenames in os.walk(root_directory):
         _, extension = os.path.splitext(filename)
 
         # Open the file and read its contents
-        with open(file_path, 'r+') as file:
+        with open(file_path, 'r') as file:
             content = file.read()
 
-            # Check if the header is already present
+            # Check for duplicate headers based on file type
             if extension in ['.cpp', '.h', '.cxx', '.cu']:
-                if license_header_c_style.strip() not in content:
-                    # Prepend the license header if not present
-                    file.seek(0, 0)
-                    file.write(license_header_c_style + content)
-            elif extension in [ '.txt', '.sh', '.py' ]:                    
-                if license_header_python_style.strip() not in content:
-                    # Prepend the license header if not present
-                    file.seek(0, 0)
-                    file.write(license_header_python_style + content)
-
-print("License headers added to all relevant files in the directory tree, if not already present.")
+                if content.count(license_header_c_style.strip()) > 1:
+                    print(f"Duplicate header found in file: {file_path}")
+            elif extension in [ '.txt', '.sh', '.py' ]:                
+                if content.count(license_header_python_style.strip()) > 1:
+                    print(f"Duplicate header found in file: {file_path}")
