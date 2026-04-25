fd442938165364d15b8681e656f8e798eabbf26a
lafourcadep <lafourcadepaul16@gmail.com>
2026-04-25 16:06:48 +0200

    Resolve problems in wall tests. wall and move_wall operators are now included. Renamed move_wall and wall operators as well due to obsolete use of v2 extension

--- src/compute/move_wall.cpp
+++ src/compute/move_wall.cpp
@@ -28,27 +28,27 @@ namespace exaStamp
 {
   using namespace exanb;
 
-  class move_wall_v2 : public OperatorNode
+  class MoveWall : public OperatorNode
   {
-   
-    ADD_SLOT( Vec3d  , init_normal , INPUT , Vec3d{1.0,0.0,0.0} );
-    ADD_SLOT( double , init_offset , INPUT , 0.0 );
-    ADD_SLOT( double , init_cutoff , INPUT , REQUIRED );
-    ADD_SLOT( double , init_epsilon , INPUT , REQUIRED );
-    ADD_SLOT( double , init_time , INPUT , REQUIRED );
-    ADD_SLOT( double , init_velocity , INPUT , REQUIRED );
-    ADD_SLOT( long , init_exponent , INPUT , 12 );
-    ADD_SLOT( double , physical_time , INPUT , REQUIRED );
+
+    ADD_SLOT(Vec3d, init_normal, INPUT, Vec3d{1.0, 0.0, 0.0});
+    ADD_SLOT(double, init_offset, INPUT, 0.0);
+    ADD_SLOT(double, init_cutoff, INPUT, REQUIRED);
+    ADD_SLOT(double, init_epsilon, INPUT, REQUIRED);
+    ADD_SLOT(double, init_time, INPUT, REQUIRED);
+    ADD_SLOT(double, init_velocity, INPUT, REQUIRED);
+    ADD_SLOT(long, init_exponent, INPUT, 12);
+    ADD_SLOT(double, physical_time, INPUT, REQUIRED);
 
     // outputs for walll
-    ADD_SLOT( Vec3d  , normal , OUTPUT );
-    ADD_SLOT( double , offset , OUTPUT );
-    ADD_SLOT( double , cutoff , OUTPUT );
-    ADD_SLOT( double , epsilon , OUTPUT );
-    ADD_SLOT( long , exponent , OUTPUT );
-  public:
+    ADD_SLOT(Vec3d, normal, OUTPUT);
+    ADD_SLOT(double, offset, OUTPUT);
+    ADD_SLOT(double, cutoff, OUTPUT);
+    ADD_SLOT(double, epsilon, OUTPUT);
+    ADD_SLOT(long, exponent, OUTPUT);
 
-    inline void execute () override final
+  public:
+    inline void execute() override final
     {
       *normal = *init_normal;
       *cutoff = *init_cutoff;
@@ -56,8 +56,7 @@ namespace exaStamp
       *offset = *init_offset;
       *exponent = *init_exponent;
 
-
-      if( *physical_time >=  *init_time)
+      if (*physical_time >= *init_time)
       {
         *offset = *init_offset + (*physical_time - *init_time) * (*init_velocity);
       }
@@ -66,16 +65,14 @@ namespace exaStamp
       {
         *offset = *init_offset;
       }
-   	ldbg<<"offset="<<*offset<<std::endl;
+      ldbg << "offset=" << *offset << std::endl;
     }
-
   };
-  
-  // === register factories ===  
-  ONIKA_AUTORUN_INIT(move_wall_V2)
+
+  // === register factories ===
+  ONIKA_AUTORUN_INIT(move_wall)
   {
-    OperatorNodeFactory::instance()->register_factory( "move_wall_v2", make_simple_operator< move_wall_v2 > );
+    OperatorNodeFactory::instance()->register_factory("move_wall", make_simple_operator<MoveWall>);
   }
 
 }
-
