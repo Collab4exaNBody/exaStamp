d5ae76e6061c41c5e3b10844090f3202b208697b
Paul Lafourcade <paul.lafourcade@cea.fr>
2026-07-09 17:52:04 +0200

    Revert new functors, use template functor from exaNBody instead for fnorm and fnorm2

--- src/compute/include/exaStamp/compute/physics_functors.h
+++ src/compute/include/exaStamp/compute/physics_functors.h
@@ -65,24 +65,6 @@ namespace exaStamp
       return this->operator() (vx,vy,vz,m_type);
     }
   };
-
-  struct VelocityNormFunctor
-  {
-    const ParticleSpecie* __restrict__ m_species = nullptr;
-    ONIKA_HOST_DEVICE_FUNC inline double operator () (double vx, double vy, double vz) const
-    {
-      return sqrt(vx*vx+vy*vy+vz*vz);
-    }
-  };
-
-  struct ForceNormFunctor
-  {
-    const ParticleSpecie* __restrict__ m_species = nullptr;
-    ONIKA_HOST_DEVICE_FUNC inline double operator () (double fx, double fy, double fz) const
-    {
-      return sqrt(fx*fx+fy*fy+fz*fz);
-    }
-  };
   
   struct MassFunctor
   {
