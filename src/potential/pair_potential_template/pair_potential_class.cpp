22ab61118c5fa9340e7c93157a3ef41b8a032da1
Thierry CARRARD <Thierry.CARRARD@cea.fr>
2026-07-23 11:45:34 +0200

    fixed retro-compatibility issue

--- src/potential/pair_potential_template/pair_potential_class.cpp
+++ src/potential/pair_potential_template/pair_potential_class.cpp
@@ -19,7 +19,6 @@ under the License.
 
 #include <exaStamp/potential_factory/pair_potential_factory.h>
 #include <exaStamp/potential_factory/pair_potential.h>
-#include <onika/memory/mm_vector_yaml.h>
 
 #undef USTAMP_POTENTIAL_WITH_VIRIAL
 #include "pair_potential_force_op.h"
