f539dfeda2a94cf46ff1c928d53a141569ddfdb3
lafourcadep <lafourcadepaul16@gmail.com>
2026-07-28 20:17:41 +0200

    Add a .csv histogram writer

--- src/analysis_particle/compute_local_mechanical_metrics.cpp
+++ src/analysis_particle/compute_local_mechanical_metrics.cpp
@@ -34,8 +34,8 @@ under the License.
 #include <exanb/defbox/deformation_yaml.h>
 #include <exanb/defbox/deformation_math.h>
 
-#include <exaStamp/mechanical/cell_particles_local_mechanical_metrics.h>
-#include <exaStamp/mechanical/compute_local_mechanical_metrics.h>
+#include <exaStamp/analysis_particle/cell_particles_local_mechanical_metrics.h>
+#include <exaStamp/analysis_particle/compute_local_mechanical_metrics.h>
 
 #include <exanb/core/config.h>
 #include <exanb/particle_neighbors/chunk_neighbors.h>
